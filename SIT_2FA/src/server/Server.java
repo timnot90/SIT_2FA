96f22056a7e3772f37f818ffe9e26032f69f0c76
timnot90 <timo.notheisen@googlemail.com>
2014-05-21 15:53:48 +0200

    test

--- SIT_2FA/src/server/Server.java
+++ SIT_2FA/src/server/Server.java
@@ -4,7 +4,7 @@ import java.net.InetSocketAddress;
 
 import server.handler.DefaultHandler;
 import server.handler.DiffieHellmanHandler;
-import server.handler.LoginHandler;
+import server.handler.TokenHandler;
 import server.handler.PollingHandler;
 
 import com.sun.net.httpserver.HttpServer;
@@ -13,9 +13,9 @@ public class Server {
 
 	public static void main(String[] args) throws Exception {
 		HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-		server.createContext("/", new DefaultHandler());
+		server.createContext("/Init", new DefaultHandler());
 		server.createContext("/DifHel", new DiffieHellmanHandler());
-		server.createContext("/Login", new LoginHandler());
+		server.createContext("/Token", new TokenHandler());
 		server.createContext("/Polling", new PollingHandler());
 		server.setExecutor(null); // creates a default executor
 		server.start();
--- SIT_2FA/src/server/handler/DefaultHandler.java
+++ SIT_2FA/src/server/handler/DefaultHandler.java
@@ -16,17 +16,16 @@ public class DefaultHandler implements HttpHandler {
 		try {
 			response.put("name", "foo");
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 		try {
 			httpExchange.getResponseHeaders().set("content-type", "application/json");
 			httpExchange.sendResponseHeaders(200, response.toString().length());
-			OutputStream os = httpExchange.getResponseBody();
+			OutputStream outputStream = httpExchange.getResponseBody();
 			
-			os.write(response.toString().getBytes("utf-8"));
-			os.close();
+			outputStream.write(response.toString().getBytes("utf-8"));
+			outputStream.close();
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
