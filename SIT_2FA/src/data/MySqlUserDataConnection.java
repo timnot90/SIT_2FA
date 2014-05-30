eb5603c5f3b2dc983cbabafb8596c009dda23dac
zinsoldat <reneschmitt@gmx.de>
2014-05-30 15:32:26 +0200

    jdbc treiber eingebunden

--- SIT_2FA/src/data/MySqlUserDataConnection.java
+++ SIT_2FA/src/data/MySqlUserDataConnection.java
@@ -3,27 +3,35 @@ package data;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 
-public class MySqlUserDataConnection implements UserDataInterface{
-	private String username ="sit";
-	private String password = "qwer1234";
+public class MySqlUserDataConnection implements UserDataInterface {
+	private String username = "root";
+	private String password = "";
 	private String address = "127.0.0.1";
 	private String schema = "sit";
-	
+
 	private Connection connection;
-		
+
 	public MySqlUserDataConnection() {
-	      // this will load the MySQL driver, each DB has its own driver
-	      try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection(address, username, password);
-			connection.setSchema(schema);
+		// this will load the MySQL driver, each DB has its own driver
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			connection = DriverManager.getConnection("jdbc:mysql://" 
+					+ address + "/" + schema + "?user=" + username 
+					+ "&password=" + password);
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -33,16 +41,16 @@ public class MySqlUserDataConnection implements UserDataInterface{
 	public boolean createUser(String username, String password, String secret,
 			String salt) {
 		try {
-			PreparedStatement userCreationStatement = 
-					connection.prepareStatement("INSERT INTO "
+			PreparedStatement userCreationStatement = connection
+					.prepareStatement("INSERT INTO "
 							+ "users(username, password, secret, salt) "
 							+ "VALUES(?,?,?,?)");
-			
+
 			userCreationStatement.setString(1, username);
 			userCreationStatement.setString(2, password);
 			userCreationStatement.setString(3, secret);
 			userCreationStatement.setString(4, salt);
-			
+
 			return userCreationStatement.execute();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -54,13 +62,25 @@ public class MySqlUserDataConnection implements UserDataInterface{
 	@Override
 	public void setToken(String token) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public String getPassword(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		String password = "";
+		String statement = "SELECT password FROM users WHERE username=?";
+		try {
+			PreparedStatement userSelectionStatement =
+					connection.prepareStatement(statement);
+			userSelectionStatement.setString(1, username);
+			ResultSet rs = userSelectionStatement.executeQuery(statement);
+			rs.next();
+			password = rs.getString("password");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return password;
 	}
 
 	@Override
